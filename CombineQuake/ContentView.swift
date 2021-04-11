1d1b0265cbebab8b5f347617c168f44e8ae34bcc
AdrianBinDC <adrian.bolinger@me.com>
2021-04-11 03:08:54 -0400

    Simplify ContentView

--- CombineQuake/ContentView.swift
+++ CombineQuake/ContentView.swift
@@ -11,35 +11,28 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var viewModel = ContentViewModel()
-                    
+    
     init(viewModel: ContentViewModel) {
         self.viewModel = viewModel
     }
-        
+    
     var body: some View {
-        ZStack(alignment: .top) {
-            VStack {
-                HStack {
-                    Text(viewModel.dateSpanString)
-                }
-                
-                Picker("Date Span", selection: $viewModel.dateSpan) {
-                    ForEach(DateSpan.allCases, id: \.rawValue) { value in
-                        Text(value.description)
-                            .tag(value)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .onChange(of: /*@START_MENU_TOKEN@*/"Value"/*@END_MENU_TOKEN@*/, perform: { value in
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                })
-
-
-                                
-                DatePicker("Selected Date",
-                           selection: $viewModel.endDate)
-                    .datePickerStyle(GraphicalDatePickerStyle())
+        VStack {
+            HStack {
+                Text(viewModel.dateSpanString)
             }
+            
+            Picker("Date Span", selection: $viewModel.dateSpan) {
+                ForEach(DateSpan.allCases, id: \.rawValue) { value in
+                    Text(value.description)
+                        .tag(value)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            DatePicker("Selected Date",
+                       selection: $viewModel.endDate)
+                .datePickerStyle(GraphicalDatePickerStyle())
         }
     }
 }
@@ -48,6 +41,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let viewModel = ContentViewModel()
         ContentView(viewModel: viewModel)
-//        ContentView(viewModel: viewModel)
+        //        ContentView(viewModel: viewModel)
     }
 }
